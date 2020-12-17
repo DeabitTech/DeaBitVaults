51893a46f013aad14d8a4f42bf3ad6a3ce24041f
Deabit <info@deabit.net>
2020-12-17 18:23:57 +0100

    Update VaultManager.js

--- src/components/VaultManager.js
+++ src/components/VaultManager.js
@@ -5,11 +5,11 @@ import { McdPlugin } from '@makerdao/dai-plugin-mcd';
 
 export const VaultManager = () =>{
     const [maker,setMaker] = useState()
-    let myPrivateKey = '09C6765DF3A23CCC05B7F3B48BFA07C48522C4C1D2F2404B11BDDB14F2CA4FBE'
+    let myPrivateKey = process.env.PRIV_KEY
     const istanceVaultManager = async () =>{
         const maker = await Maker.create('http', {
             plugins: [McdPlugin],
-            url: 'https://kovan.infura.io/v3/e90a29f8a5474442b83e287a590a60dc',
+            url: `https://kovan.infura.io/v3/${process.env.INFURA_ID}`,
             privateKey: myPrivateKey
         });
         setMaker(maker)
